877b8c3e5df74af9ef8323ae4d483ff998a9cd2e
Andreas19 <andreas19@posteo.eu>
2022-01-24 15:01:33 +0100

    Shorter implementations

--- Python/fizzbuzz7.py
+++ Python/fizzbuzz7.py
@@ -1,1 +1,1 @@
-print(', '.join('FizzBuzz' if i % 15 == 0 else 'Fizz' if i % 3 == 0 else 'Buzz' if i % 5 == 0 else str(i) for i in range(1, 101)))
+print(', '.join(((str(i) if i % 5 else 'Buzz') if i % 3 else 'Fizz') if i % 15 else 'FizzBuzz' for i in range(1, 101)))
